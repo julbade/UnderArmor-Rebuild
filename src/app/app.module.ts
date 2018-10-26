7b14ba6752d8e676b442a045c84e3876c9fd1e51
julius bade <julbade21@gmail.com>
2018-10-26 10:46:26 -0700

    it will add a seconday navbar on top

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { UnderNavbarComponent } from './under-navbar/under-navbar.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent
+    NavBarComponent,
+    UnderNavbarComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/under-navbar/under-navbar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-under-navbar',
+  templateUrl: './under-navbar.component.html',
+  styleUrls: ['./under-navbar.component.css']
+})
+export class UnderNavbarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
