b99e0ae545b0ad66df07c27ad6b766968174b158
julius bade <julbade21@gmail.com>
2018-11-10 13:11:26 -0800

    remove apikey.ts t git ignore

---
+++ src/app/api-keys.ts
@@ -0,0 +1,8 @@
+export const masterFirebaseConfig = {
+  apiKey: "AIzaSyBkQJJYAEL8JeNWZdoTQRSmFmUGlHLdFP4",
+  authDomain: "underarmour-rebuild-6546c.firebaseapp.com",
+  databaseURL: "https://underarmour-rebuild-6546c.firebaseio.com",
+  projectId: "underarmour-rebuild-6546c",
+  storageBucket: "underarmour-rebuild-6546c.appspot.com",
+  messagingSenderId: "261452863157"
+}
