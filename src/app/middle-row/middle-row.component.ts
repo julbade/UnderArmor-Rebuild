58d21e0f999c9710a79999b75da90fd00c11ac94
julius bade <julbade21@gmail.com>
2019-01-06 23:14:52 -0800

    fixed responsive design for webpage

--- src/app/middle-row/middle-row.component.ts
+++ src/app/middle-row/middle-row.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-middle-row',
   templateUrl: './middle-row.component.html',
   styleUrls: ['./middle-row.component.css']
 })
-export class MiddleRowComponent implements OnInit {
+export class MiddleRowComponent {
 
   constructor() { }
 
-  ngOnInit() {
+  responsiveNavbar() {
+  let x = document.getElementById('navLinks');
+  if (x.className === 'navLinks') {
+    x.className += ' responsive';
+  }  else {
+    x.className = 'navLinks';
+    }
+    let y = document.getElementById('toggle');
+    if(y.className === 'fa fa-toggle-down') {
+      y.className += 'fa fa-toggle-up';
+    } else {
+      y.className = 'fa fa-toggle-down';
+    }
   }
 
 }
